59bc308b282aec6aa7a7bb5595887e53eb2e60b7
tyron ferreira <tyron@MBP-de-tyron.lan>
2025-11-02 17:39:39 +0100

    feat: join on geohash

--- models/intermediate/stg_magasin.sql
+++ models/intermediate/stg_magasin.sql
@@ -1,18 +1,34 @@
 {{ config(materialized='table') }}
 
 
-WITH combined AS (
+WITH GI_SOURCE AS (
 SELECT *,
-       ST_GEOHASH(TO_GEOGRAPHY(CONCAT('POINT(', longitude, ' ', latitude, ')'))) AS geohash
+       ST_GEOHASH(TO_GEOGRAPHY(CONCAT('POINT(', longitude, ' ', latitude, ')')), 6) AS geohash
 FROM DTL_EXO.GI.MAGASINS
-UNION
+),
+
+TI_SOURCE AS (
 SELECT *,
-       ST_GEOHASH(TO_GEOGRAPHY(CONCAT('POINT(', longitude, ' ', latitude, ')'))) AS geohash
+       ST_GEOHASH(TO_GEOGRAPHY(CONCAT('POINT(', longitude, ' ', latitude, ')')), 6) AS geohash
 FROM DTL_EXO.GI.MAGASINS
+),
+
+combined AS (
+SELECT
+    gim.geohash,
+    gim.id,
+    gim.name,
+    gim.latitude,
+    gim.longitude
+FROM GI_SOURCE gim
+INNER JOIN TI_SOURCE tis
+ON gim.geohash = tis.geohash
+AND gim.name = tis.name
 )
 
 SELECT
     '{{ run_started_at }}'::timestamp as dbt_updated_at,
+    geohash,
     id,
     name,
     latitude,
