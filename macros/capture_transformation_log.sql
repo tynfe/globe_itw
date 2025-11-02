bcb97392da5f76d7f66f1e3bacecd38dfd514e5c
tyron ferreira <tyron@MBP-de-tyron.lan>
2025-11-02 20:46:13 +0100

    feat: add testing to validate geohash, rework macro to capture log

--- macros/capture_transformation_log.sql
+++ macros/capture_transformation_log.sql
@@ -1,32 +1,39 @@
 {% macro capture_transformation_log_metadata() %}
-    {% set query %}
-        INSERT INTO {{ target.database }}.{{ target.schema }}.TRANSFORMATION_LOGS (
-            log_id,
-            model_name,
-            schema_name,
-            database_name,
-            run_started_at,
-            run_completed_at,
-            rows_affected,
-            status,
-            materialization,
-            target_name,
-            invocation_id
-        ) VALUES (
-            UUID_STRING(),
-            '{{ this.name }}',
-            '{{ this.schema }}',
-            '{{ this.database }}',
-            '{{ run_started_at }}'::TIMESTAMP_NTZ,
-            CURRENT_TIMESTAMP(),
-            (SELECT COUNT(*) FROM {{ this }}),
-            'SUCCESS',
-            '{{ config.get("materialized") }}',
-            '{{ target.name }}',
-            '{{ invocation_id }}'
-        );
-    {% endset %}
+    {% if execute %}
+        {% set query %}
+            INSERT INTO {{ target.database }}.{{ target.schema }}.TRANSFORMATION_LOGS (
+                model_name,
+                schema_name,
+                database_name,
+                run_started_at,
+                run_completed_at,
+                execution_duration_seconds,
+                rows_affected,
+                target_name,
+                invocation_id,
+                status,
+                error_message,
+                run_by,
+                created_at
+            )
+            SELECT
+                '{{ this.name }}',
+                '{{ this.schema }}',
+                '{{ this.database }}',
+                '{{ run_started_at }}'::TIMESTAMP_NTZ,
+                CURRENT_TIMESTAMP(),
+                TIMESTAMPDIFF(SECOND, '{{ run_started_at }}'::TIMESTAMP_NTZ, CURRENT_TIMESTAMP()),
+                (SELECT COUNT(*) FROM {{ this }}),
+                '{{ target.name }}',
+                '{{ invocation_id }}',
+                'SUCCESS',
+                'NULL',
+                CURRENT_USER(),
+                CURRENT_TIMESTAMP()
+            ;
+        {% endset %}
 
-    {% do run_query(query) %}
-    {% do log("✅ Metadata captured for " ~ this.name, info=true) %}
+        {% do run_query(query) %}
+        {% do log("✅ Metadata captured for " ~ this.name, info=true) %}
+    {% endif %}
 {% endmacro %}
--- tests/test_geohashes_duplicate.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    geohash_150m,
-    COUNT(*) AS nb_magasins,
-    LISTAGG(nom, ', ') AS liste_magasins
-FROM {{ ref('stg_magasin') }}
-GROUP BY geohash_1km
-HAVING COUNT(*) > 5
